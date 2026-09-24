bbcd332cbb7ed35c9dcbd3a6d199db4916402dfe
Luanhts <luanh3nrique08@gmail.com>
2026-09-24 11:25:36 -0300

    feat: db config

--- src/db/main.ts
+++ src/db/main.ts
@@ -1,9 +1,22 @@
-import {Client} from 'pg';
+import {Pool} from 'pg';
+import dotenv from 'dotenv';
 
-const db = new Client({
-    host:"localhost",
-    user:"postgres",
-    port:5432,
-    password:"password",
-    database:"telemetry"
+dotenv.config();
+
+const db = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    port: Number(process.env.DB_PORT),
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
+
+db.on("connect", () => {
+    console.log("Connected to the database");
+});
+
+db.on("error", (err) => {
+    console.error("Error connecting to the database", err);
+});
+
+export default db;
